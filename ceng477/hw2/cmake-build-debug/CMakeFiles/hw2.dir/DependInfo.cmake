c11f884b939d10e696cdab9cbdc54d6664914a5b
Deniz Polat <denizpolat@protonmail.com>
2020-12-15 02:03:58 +0300

    Add files via upload

---
+++ ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/camera.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/camera.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/image.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/image.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/integrator.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/integrator.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/jpeg.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/jpeg.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/main.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/main.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/material.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/material.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/matrix.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/matrix.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/mesh.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/mesh.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/parser.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/parser.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/point_light.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/point_light.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/ppm.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/ppm.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/ray.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/ray.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/rotation.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/rotation.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/sample_main_files/main_for_jpeg_to_ppm.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/sample_main_files/main_for_jpeg_to_ppm.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/sample_main_files/main_for_matrix_inversion.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/sample_main_files/main_for_matrix_inversion.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/sample_main_files/main_for_write_jpeg_file.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/sample_main_files/main_for_write_jpeg_file.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/scaling.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/scaling.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/scene.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/scene.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/sphere.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/sphere.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/texture.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/texture.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/timer.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/timer.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/tinyxml2.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/tinyxml2.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/transformation.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/transformation.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/translation.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/translation.cpp.o"
+  "/home/deniz/Downloads/fall 2020/ceng477/hw2/triangle.cpp" "/home/deniz/Downloads/fall 2020/ceng477/hw2/cmake-build-debug/CMakeFiles/hw2.dir/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
